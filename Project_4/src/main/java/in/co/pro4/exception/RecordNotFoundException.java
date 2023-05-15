4321a25f2efbd2b39801cdcdeed59d4733472260
shaswatmandre <smandre77@gmail.com>
2023-05-15 13:38:00 +0530

    ok

---
+++ Project_4/src/main/java/in/co/pro4/exception/RecordNotFoundException.java
@@ -0,0 +1,13 @@
+package in.co.pro4.exception;
+
+/**
+ * RecordNotFoundException thrown when a record not found occurred.
+ * @author Shaswat Mandre
+ * 
+ */
+public class RecordNotFoundException extends Exception {
+
+	public RecordNotFoundException(String msg) {
+		super(msg);
+	}
+}
